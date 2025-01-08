5728eaebe8337c6f29cf050d3fa4edd067a16a3e
afoquendo <afoquendou@gmail.com>
2025-01-08 09:54:25 -0500

    Formatted response in emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,6 +2,7 @@
 This module defines a function to call the Emotion Predict API from the watson NLP Library
 """
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     """
@@ -21,5 +22,7 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header, timeout=10)
-
-    return response.text
+    formatted_response = json.loads(response.text)
+    scores = formatted_response['emotionPredictions'][0]['emotion']
+    scores['dominant_emotion'] = max(scores, key=scores.get)
+    return scores
